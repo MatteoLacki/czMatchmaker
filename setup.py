c11e84cb3439dfd22fcc44adc57baf4df2bb682a
Matteo Lacki <matteo.lacki@gmail.com>
2017-02-01 17:34:33 +0100

    setup ready

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ setup(
     packages      = ['czMatchmaker'], # this must be the same as the name above
     version       = '0.1',
     description   = 'Maximum likelihood estimates of electron transfer reaction parameters.',
-    author        = 'Mateusz Lacki',
+    author        = 'Mateusz Krzysztof Lacki',
     author_email  = 'matteo.lacki@gmail.com',
     url           = 'https://github.com/MatteoLacki/linearCounter',
     download_url  = 'https://github.com/MatteoLacki/linearCounter.git',
-    keywords      = ['linear operations', 'abstract linear spaces', 'Counter'],
-    classifiers   = [],
+    keywords      = ['Matching c and z ions', 'Mass Spectrometry'],
+    license       = 'GNU AFFERO GENERAL PUBLIC LICENSE v3',
+    classifiers   = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry'
+    ],
+    install_requires = ['dplython' , 'pandas']
 )
