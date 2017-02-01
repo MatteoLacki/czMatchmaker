3876fc9f511a076a7b73a6aaf8cd985b4587f3a7
Matteo Lacki <matteo.lacki@gmail.com>
2017-02-01 17:29:53 +0100

    Trying to make a module.

--- czMatchmaker/__init__.py
+++ czMatchmaker/__init__.py
@@ -16,16 +16,4 @@
 #   Version 3 along with czMatchmaker.  If not, see
 #   <https://www.gnu.org/licenses/agpl-3.0.en.html>.
 
-from dplython import DelayFunction
-
-@DelayFunction
-def Round_and_Multi(x, times = 100):
-    return [ int(round(a*times)) for a in x ]
-
-@DelayFunction
-def Round(x):
-    return [ round(a) for a in x ]
-
-@DelayFunction
-def crossprod( x, y):
-    return [ xx*yy for xx,yy in zip(x,y) ]
+from czMatchmaker import czMatchmaker
